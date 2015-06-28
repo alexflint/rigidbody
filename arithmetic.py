0f27c66b3154d63d935f82051b636861116cb185
Alex Flint <alex.flint@gmail.com>
2015-06-28 14:27:16 -0700

    add arithmetic module

---
+++ arithmetic.py
@@ -0,0 +1,122 @@
+import functools
+import numpy as np
+
+
+def pr(a):
+    """
+    For vectors A, divide each element of A by the last.
+    For matrices, divide each row by its rightmost element.
+    For arrays of high dimension, project along the last dimension.
+    """
+    a = np.asarray(a)
+    return a[..., :-1] / a[..., -1:]
+
+
+def unpr(a):
+    """
+    For vectors A, return A with 1 appended.
+    For matrices, append a column of ones.
+    For arrays of higher dimension, append along the last dimension.
+    """
+    a = np.asarray(a)
+    col_shape = a.shape[:-1] + (1,)
+    return np.concatenate((a, np.ones(col_shape)), axis=-1)
+
+
+def normalized(a):
+    """
+    For vertors A, return a unit vector in the direction of A.
+    For matrices, return a matrix of the same size where each row has unit norm.
+    For arrays of higher dimension, normalize along the last axis.
+    """
+    a = np.asarray(a)
+    return a / np.sqrt(np.sum(np.square(a), axis=-1))[..., None]
+
+
+def unreduce(a, mask, fill=0.):
+    """
+    Create a vector in which the positions set to True in the mask vector are set to sequential values from A.
+    """
+    a = np.asarray(a)
+    mask = np.asarray(mask)
+    x = np.repeat(fill, len(mask)).astype(a.dtype)
+    x[mask] = a
+    return x
+
+
+def unreduce_2d(a, mask, fill=0.):
+    """
+    Create a matrix in which the rows and columns set to True in the mask vector are set to the entries from A.
+    """
+    a = np.asarray(a)
+    x = np.ones((len(mask), len(mask))) * fill
+    x[np.ix_(mask, mask)] = a
+    return x
+
+
+def sumsq(a, axis=None):
+    """
+    Compute the sum of squares.
+    """
+    return np.sum(np.square(a), axis=axis)
+
+
+def skew(a):
+    """
+    Compute the skew-symmetric matrix for a. The returned matrix M has the property 
+    that for any 3-vector v, M * v = a x v where x denotes the cross product.
+    """
+    a = np.asarray(a)
+    assert a.shape == (3,), 'shape was was %s' % str(a.shape)
+    return np.array([[0, -a[2], a[1]],
+                     [a[2], 0, -a[0]],
+                     [-a[1], a[0], 0.]])
+
+
+def unit(i, n):
+    """
+    Compute a unit vector along the i-th axis in N dimensions.
+    """
+    return (np.arange(n) == i).astype(float)
+
+
+def orthonormalize(a):
+    """
+    Find the closest orthonormal matrix to R.
+    Note that the returned matrix may have determinant +1 or -1, so the result
+    may be either a rotation or a rotoinversion.
+    """
+    u, _, v = np.linalg.svd(a)
+    return np.dot(u, v)
+
+
+def minmedmax(a):
+    """
+    Compute [min(x), median(a), max(a)]
+    """
+    if len(a) == 0:
+        raise Exception('warning [utils.minmedmax]: empty list passed')
+    else:
+        return np.min(a), np.median(a), np.max(a)
+
+
+def cis(th):
+    """
+    Compute [cos(th), sin(th)].
+    """
+    return np.array((np.cos(th), np.sin(th)))
+
+
+def dots(*a):
+    """
+    Multiply an arbitrary number of matrices with np.dot.
+    """
+    return functools.reduce(np.dot, a)
+
+
+def spy(a, t=1e-5):
+    """
+    Return a string representation of the sparsity pattern of A.
+    """
+    a = np.atleast_2d(a)
+    return '\n'.join(map(lambda row: '['+''.join('x' if abs(val)>t else ' ' for val in row)+']', a))
---
+++ arithmetic_test.py
@@ -0,0 +1,66 @@
+import unittest
+import numpy as np
+from numpy.testing import assert_array_almost_equal
+
+import arithmetic
+
+class ArithmeticTest(unittest.TestCase):
+	def test_normalized(self):
+		a = [2., 0., 0.]
+		assert_array_almost_equal(arithmetic.normalized(a), [1., 0., 0.])
+
+	def test_pr(self):
+		a = [4., 6., 2.]
+		assert_array_almost_equal(arithmetic.pr(a), [2., 3.])
+
+	def test_unpr(self):
+		a = [1., 2.]
+		assert_array_almost_equal(arithmetic.unpr(a), [1., 2., 1.])
+
+	def test_spy(self):
+		a = [[1, 0], [1, 1]]
+		self.assertEqual(arithmetic.spy(a), '[x ]\n[xx]')
+
+	def test_unreduce(self):
+		a = [1, 2]
+		mask = [False, True, False, True, False]
+		assert_array_almost_equal(arithmetic.unreduce(a, mask), [0, 1, 0, 2, 0])
+
+	def test_unreduce_2d(self):
+		a = [[1, 2], [3, 4]]
+		mask = [True, False, True]
+		expected = [
+			[1, 0, 2],
+			[0, 0, 0],
+			[3, 0, 4]]
+		assert_array_almost_equal(arithmetic.unreduce_2d(a, mask), expected)
+
+	def test_dots(self):
+		a = np.eye(2)
+		b = np.array([[1., 2.], [3., 4.]])
+		assert_array_almost_equal(arithmetic.dots(a, b, a), b)
+
+	def test_sumsq(self):
+		a = [1, 2]
+		assert_array_almost_equal(arithmetic.sumsq(a), 5)
+
+	def test_skew(self):
+		a = [1, 2, 3]
+		expected = [
+			[0, -3, 2],
+			[3, 0, -1],
+			[-2, 1, 0]]
+		assert_array_almost_equal(arithmetic.skew(a), expected)
+
+	def test_unit(self):
+		assert_array_almost_equal(arithmetic.unit(1, 5), [0, 1, 0, 0, 0])
+
+	def test_min_med_max(self):
+		a = [1, 2, 3, 4, 5]
+		assert_array_almost_equal(arithmetic.minmedmax(a), [1, 3, 5])
+
+
+
+
+if __name__ == '__main__':
+	unittest.main()
