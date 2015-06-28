4382318d6e8442f291139c8d8c10d879d083d452
Alex Flint <alex.flint@gmail.com>
2015-06-28 16:26:30 -0700

    add distribution stuff

---
+++ __init__.py
@@ -0,0 +1,7 @@
+from .arithmetic import \
+	pr, unpr, normalized, unreduce, unreduce_2d, sumsq, skew, \
+	unit, orthonormalize, minmedmax, cis, dots, spy
+
+from .group import SO3, SE3
+
+from .array_comparison import assert_arrays_almost_equal
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='rigidbody',
+      description='Rigid body transforms in 3D',
+      version='0.1',
+      author='Alex Flint',
+      author_email='alex.flint@gmail.com',
+      url='...',
+      packages=['rigidbody'],
+      package_dir={'rigidbody': '.'},
+      )
