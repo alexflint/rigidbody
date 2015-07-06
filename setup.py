4db449504e4682c5fa65c43a6bd1b6db3aa20c63
Alex Flint <alex.flint@gmail.com>
2015-07-05 20:21:28 -0700

    Release version 0.12

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(name='rigidbody',
       description='Rigid body transforms in 3D',
-      version='0.11',
+      version='0.12',
       author='Alex Flint',
       author_email='alex.flint@gmail.com',
       url='https://github.com/alexflint/rigidbody',
